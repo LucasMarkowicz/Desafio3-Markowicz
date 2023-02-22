632a53ab2e5fb40ceff81680baef77c76d296f67
LucasMarkowicz <luckpelle1@gmail.com>
2023-02-21 23:47:15 -0300

    cambios websockets

--- src/index.js
+++ src/index.js
@@ -122,23 +122,20 @@ const socketIO = require('socket.io');
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// Manejar las conexiones de los clientes
+
 io.on('connection', socket => {
   console.log('Cliente conectado');
 
-  // Manejar la desconexión de los clientes
   socket.on('disconnect', () => {
     console.log('Cliente desconectado');
   });
 });
 
-// Agregar la ruta para la vista de productos en tiempo real
 app.get('/realtimeproducts', (req, res) => {
   const products = manager.getProducts();
   res.render('realTimeProducts', { products });
 });
 
-// Iniciar el servidor
 server.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
 });
